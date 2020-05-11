4dce218892cf06948b9ab0c59d4a21f218d93753
Dominik Zilliken <admin@eggrade.de>
2020-05-11 22:22:41 +0200

    Frame for IValidationAdapter implementation

---
+++ ValidationAdapter/ValidationAdapter.AspNetCore.UnitTests/Adapters/AspNetCoreValidationAdapterTests.cs
@@ -0,0 +1,18 @@
+﻿using BanallyMe.ValidationAdapter.AspNetCore.Adapters;
+using Microsoft.AspNetCore.Http;
+using Moq;
+
+namespace BanallyMe.ValidationAdapter.AspNetCore.UnitTests.Adapters
+{
+    public class AspNetCoreValidationAdapterTests
+    {
+        private readonly Mock<IHttpContextAccessor> contextAccessorMock;
+        private readonly AspNetCoreValidationAdapter testedAdapter;
+
+        public AspNetCoreValidationAdapterTests()
+        {
+            contextAccessorMock = new Mock<IHttpContextAccessor>();
+            testedAdapter = new AspNetCoreValidationAdapter(contextAccessorMock.Object);
+        }
+    }
+}
---
+++ ValidationAdapter/ValidationAdapter.AspNetCore/Adapters/AspNetCoreValidationAdapter.cs
@@ -0,0 +1,39 @@
+﻿using BanallyMe.ValidationAdapter.Adapters;
+using BanallyMe.ValidationAdapter.ValidationResults;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+
+namespace BanallyMe.ValidationAdapter.AspNetCore.Adapters
+{
+    /// <summary>
+    /// Implementation of IValidationAdapter for the ASP.NET Core framework.
+    /// </summary>
+    public class AspNetCoreValidationAdapter : IValidationAdapter
+    {
+        private readonly IHttpContextAccessor contextAccessor;
+
+        public AspNetCoreValidationAdapter(IHttpContextAccessor contextAccessor)
+        {
+            this.contextAccessor = contextAccessor;
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<ValidationError> GetAllValidationErrors()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<ValidationError> GetValidationErrorsAtPath(string errorPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public bool HasValidationErrors()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
