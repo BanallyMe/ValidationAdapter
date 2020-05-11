4c11470bfe055447495204cb5a4f718fcc061be3
BanallyMe <admin@eggrade.de>
2020-05-11 15:18:21 +0200

    Drafting Validation results

---
+++ ValidationAdapter/ValidationAdapter/ValidationResults/PathValidationErrorsCollection.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BanallyMe.ValidationAdapter.ValidationResults
+{
+    /// <summary>
+    /// Container for the results of the validation of a specific path to a model's property.
+    /// </summary>
+    public class PathValidationErrorsCollection : IEnumerable<string>
+    {
+        private PathValidationErrorsCollection(string path, string[] initialErrors)
+        {
+            Path = path;
+            ErrorMessages = initialErrors;
+        }
+
+        /// <summary>
+        /// Collection of all error messages for the path of this validation result.
+        /// </summary>
+        private string[] ErrorMessages { get; }
+
+        /// <summary>
+        /// Path to the model's property that this result is referring to.
+        /// </summary>
+        public string Path { get; }
+
+        /// <summary>
+        /// Count of Errors in this collection.
+        /// </summary>
+        public int Count => ErrorMessages.Length;
+
+        /// <summary>
+        /// Creates an error collection based on the passed collection of error messages for a specific model path.
+        /// </summary>
+        /// <param name="errorMessages">Collection of error messages that should be added for this path.</param>
+        /// <param name="path">Path to the model's property that this collection of errors refers to.</param>
+        /// <returns>The requested collection of errors for the path.</returns>
+        public static PathValidationErrorsCollection CreateWithErrorsAtPath(IEnumerable<string> errorMessages, string path)
+            => new PathValidationErrorsCollection(path, errorMessages.ToArray());
+
+        /// <summary>
+        /// Gets the error message at a specific index of the collection.
+        /// </summary>
+        /// <param name="index">Index for which the error message is requested.</param>
+        /// <returns>Error message at this index.</returns>
+        public string this[int index]
+        {
+            get { return ErrorMessages[index]; }
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            foreach (var errorMessage in ErrorMessages)
+            {
+                yield return errorMessage;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ ValidationAdapter/ValidationAdapter/ValidationResults/ValidationResult.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BanallyMe.ValidationAdapter.ValidationResults
+{
+    /// <summary>
+    /// Container for the result of a complete validation.
+    /// </summary>
+    public class ValidationResult
+    {
+        private ValidationResult(IEnumerable<PathValidationErrorsCollection> errors)
+        {
+            ValidationErrors = errors;
+        }
+
+        /// <summary>
+        /// Collection of all validation errors in this validation result.
+        /// </summary>
+        public IEnumerable<PathValidationErrorsCollection> ValidationErrors { get; }
+
+        /// <summary>
+        /// True if there are no errors in this validation result, false otherwise.
+        /// </summary>
+        public bool IsValid => !ValidationErrors.Any();
+
+        /// <summary>
+        /// Total count of all errors that have occured in the validation represented by this validation result.
+        /// </summary>
+        public int CountErrors => ValidationErrors.Sum(pathErrors => pathErrors.Count);
+
+        /// <summary>
+        /// Creates a validation result for a valid outcome of a validation.
+        /// </summary>
+        /// <returns>The valid result.</returns>
+        public static ValidationResult CreateValidResult()
+            => new ValidationResult(Array.Empty<PathValidationErrorsCollection>());
+
+        /// <summary>
+        /// Creates a validation result from a collection of validation errors.
+        /// </summary>
+        /// <param name="validationErrors">Errors that should be contained in this validation result.</param>
+        /// <returns>The validation result containing all passed errors.</returns>
+        public static ValidationResult CreateInvalidResultFromValidationErrors(IEnumerable<ValidationError> validationErrors)
+        {
+            var pathErrors = validationErrors.GroupBy(error => error.ErrorPath)
+                .Select(errorGroup => PathValidationErrorsCollection.CreateWithErrorsAtPath(errorGroup.Select(error => error.ErrorMessage), errorGroup.Key));
+
+            return new ValidationResult(pathErrors);
+        }
+    }
+}
