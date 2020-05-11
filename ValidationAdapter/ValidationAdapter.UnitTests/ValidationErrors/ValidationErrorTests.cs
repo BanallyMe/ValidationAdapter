04f4e3230a50af6ad7ec10609958c5508d41a32e
BanallyMe <admin@eggrade.de>
2020-05-11 10:49:09 +0200

    Added container class for validation errors

---
+++ ValidationAdapter/ValidationAdapter.UnitTests/ValidationErrors/ValidationErrorTests.cs
@@ -0,0 +1,90 @@
+﻿using BanallyMe.ValidationAdapter.ValidationErrors;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace BanallyMe.ValidationAdapter.UnitTests.ValidationErrors
+{
+    public class ValidationErrorTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("testmessage")]
+        public void CreateGlobalError_SetsErrorMessageCorrectly(string testErrorMessage)
+        {
+            var testError = ValidationError.CreateGlobalError(testErrorMessage);
+
+            testError.ErrorMessage.Should().Be(testErrorMessage);
+        }
+
+        [Fact]
+        public void CreateGlobalError_SetsEmptyPath()
+        {
+            var testError = ValidationError.CreateGlobalError("any Message");
+
+            testError.ErrorPath.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void CreateGlobalError_ThrowsExceptionIfErrorMessageIsNull()
+        {
+            Action errorCreation = () => ValidationError.CreateGlobalError(null);
+
+            errorCreation.Should().ThrowExactly<ArgumentNullException>().Which.ParamName.Should().Be("errorMessage");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("testmessage")]
+        public void CreateErrorAtPath_SetsErrorMessageCorrectly(string testErrorMessage)
+        {
+            var testError = ValidationError.CreateErrorAtPath(testErrorMessage, "any Path");
+
+            testError.ErrorMessage.Should().Be(testErrorMessage);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("testPath")]
+        public void CreateErrorAtPath_SetsErrorPathCorrectly(string testPath)
+        {
+            var testError = ValidationError.CreateErrorAtPath("any error message", testPath);
+
+            testError.ErrorPath.Should().Be(testPath);
+        }
+
+        [Fact]
+        public void CreateErrorAtPath_ThrowsExceptionIfErrorMessageIsNull()
+        {
+            Action errorCreation = () => ValidationError.CreateErrorAtPath(null, "any Path");
+
+            errorCreation.Should().ThrowExactly<ArgumentNullException>().Which.ParamName.Should().Be("errorMessage");
+        }
+
+        [Fact]
+        public void CreateErrorAtPath_ConvertsNullPathToEmptyPath()
+        {
+            var testError = ValidationError.CreateErrorAtPath("any Message", null);
+
+            testError.ErrorPath.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void IsGlobal_ReturnsTrueIfPathIsEmpty()
+        {
+            var testError = ValidationError.CreateErrorAtPath("any Message", "");
+
+            testError.IsGlobal.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(" ")]
+        [InlineData("dfgdf")]
+        public void IsGlobal_ReturnsFalseIfPathIsSet(string testErrorPath)
+        {
+            var testError = ValidationError.CreateErrorAtPath("any Message", testErrorPath);
+
+            testError.IsGlobal.Should().BeFalse();
+        }
+    }
+}
---
+++ ValidationAdapter/ValidationAdapter/ValidationErrors/ValidationError.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace BanallyMe.ValidationAdapter.ValidationErrors
+{
+    /// <summary>
+    /// Container for errors that occured while validating.
+    /// </summary>
+    public class ValidationError
+    {
+        private ValidationError(string errorMessage, string errorPath)
+        {
+            ErrorPath = errorPath;
+            ErrorMessage = errorMessage;
+        }
+
+        /// <summary>
+        /// The path to the model's property that has been considered as invalid.
+        /// </summary>
+        public string ErrorPath { get; }
+
+        /// <summary>
+        /// Validation message that expresses, while the property at path has been considered as invalid.
+        /// </summary>
+        public string ErrorMessage { get; }
+
+        /// <summary>
+        /// Returns true if this validation error is a global error, otherwise returns false.
+        /// </summary>
+        public bool IsGlobal => ErrorPath.Length == 0;
+
+        /// <summary>
+        /// Creates a validation error that cannot be assigned to a specific property of the model and is therefore considered as a global error.
+        /// </summary>
+        /// <param name="errorMessage">Message that describes this global error.</param>
+        /// <returns>Instance of the created Error.</returns>
+        public static ValidationError CreateGlobalError(string errorMessage)
+        {
+            if (errorMessage is null)
+                throw new ArgumentNullException(nameof(errorMessage));
+
+            return new ValidationError(errorMessage, "");
+        }
+
+        /// <summary>
+        /// Created a validation error thet refers to a specific property of the model which can be found at path.
+        /// </summary>
+        /// <param name="errorMessage">Message that describes this specific error.</param>
+        /// <param name="errorPath">Path to the model's property that has been consideres as invalid.</param>
+        /// <returns>Instance of the created specific error.</returns>
+        public static ValidationError CreateErrorAtPath(string errorMessage, string errorPath)
+        {
+            if (errorMessage is null)
+                throw new ArgumentNullException(nameof(errorMessage));
+
+            return new ValidationError(errorMessage, errorPath ?? "");
+        }
+    }
+}
