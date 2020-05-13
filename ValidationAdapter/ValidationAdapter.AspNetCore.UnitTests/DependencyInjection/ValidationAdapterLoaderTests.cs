2cfe5dcd535811acf139983fa43faecc863cfa29
BanallyMe <admin@eggrade.de>
2020-05-13 15:15:00 +0200

    Added a loader for adding the ASP.NET Core validation adapter to the ASP.NET Core Dependency Injection Container

---
+++ ValidationAdapter/ValidationAdapter.AspNetCore.UnitTests/DependencyInjection/ValidationAdapterLoaderTests.cs
@@ -0,0 +1,67 @@
+﻿using BanallyMe.ValidationAdapter.Adapters;
+using BanallyMe.ValidationAdapter.AspNetCore.Adapters;
+using BanallyMe.ValidationAdapter.AspNetCore.DependencyInjection;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Xunit;
+
+namespace BanallyMe.ValidationAdapter.AspNetCore.UnitTests.DependencyInjection
+{
+    public class ValidationAdapterLoaderTests
+    {
+        private readonly ServiceCollection fakeServiceCollection;
+
+        public ValidationAdapterLoaderTests()
+        {
+            fakeServiceCollection = new ServiceCollection();
+        }
+
+        [Fact]
+        public void AddAspNetCoreValidationAdapter_AddsActionContextAccessorIfNotContainedInServiceCollectionYet()
+        {
+            fakeServiceCollection.AddAspNetCoreValidationAdapter();
+
+            fakeServiceCollection.Where(ServiceIsActionContextAccessor).Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void AddAspNetCoreValidationAdapter_DoesntAddActionContextAccessorIfAlreadyContainedInServiceCollection()
+        {
+            fakeServiceCollection.AddTransient<IActionContextAccessor, ActionContextAccessor>();
+
+            fakeServiceCollection.AddAspNetCoreValidationAdapter();
+
+            fakeServiceCollection.Where(ServiceIsActionContextAccessor).Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void AddAspNetCoreValidationAdapter_AddsAspNetCoreValidationAdapter()
+        {
+            fakeServiceCollection.AddAspNetCoreValidationAdapter();
+
+            fakeServiceCollection.Where(ServiceIsAspNetCoreValidationAdapter).Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void AddAspNetCoreValidationAdapter_AddsAspNetCoreValidationAdapterOnlyOnceIfCalledMultipleTimes()
+        {
+            fakeServiceCollection.AddAspNetCoreValidationAdapter();
+            fakeServiceCollection.AddAspNetCoreValidationAdapter();
+            fakeServiceCollection.AddAspNetCoreValidationAdapter();
+
+            fakeServiceCollection.Where(ServiceIsAspNetCoreValidationAdapter).Should().HaveCount(1);
+        }
+
+        private static bool ServiceIsActionContextAccessor(ServiceDescriptor serviceDescriptor)
+            => serviceDescriptor.ServiceType == typeof(IActionContextAccessor)
+                && serviceDescriptor.ImplementationType == typeof(ActionContextAccessor)
+                && serviceDescriptor.Lifetime == ServiceLifetime.Transient;
+
+        private static bool ServiceIsAspNetCoreValidationAdapter(ServiceDescriptor serviceDescriptor)
+            => serviceDescriptor.ServiceType == typeof(IValidationAdapter)
+                && serviceDescriptor.ImplementationType == typeof(AspNetCoreValidationAdapter)
+                && serviceDescriptor.Lifetime == ServiceLifetime.Transient;
+    }
+}
---
+++ ValidationAdapter/ValidationAdapter.AspNetCore/DependencyInjection/ValidationAdapterLoader.cs
@@ -0,0 +1,26 @@
+﻿using BanallyMe.ValidationAdapter.Adapters;
+using BanallyMe.ValidationAdapter.AspNetCore.Adapters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace BanallyMe.ValidationAdapter.AspNetCore.DependencyInjection
+{
+    /// <summary>
+    /// Contains expansion methods for adding an ASP.NET Core Validation Adapter to the standard
+    /// ServiceCollection for building an ASP.NET Core dependency injection container.
+    /// </summary>
+    public static class ValidationAdapterLoader
+    {
+        /// <summary>
+        /// Adds an implementation of IValidationAdapter for the ASP.NET Core framework
+        /// to the service collection of the dependency injection container.
+        /// </summary>
+        /// <param name="services">Collection of services for building a dependency injection container.</param>
+        public static void AddAspNetCoreValidationAdapter(this IServiceCollection services)
+        {
+            services.TryAddTransient<IActionContextAccessor, ActionContextAccessor>();
+            services.TryAddTransient<IValidationAdapter, AspNetCoreValidationAdapter>();
+        }
+    }
+}
