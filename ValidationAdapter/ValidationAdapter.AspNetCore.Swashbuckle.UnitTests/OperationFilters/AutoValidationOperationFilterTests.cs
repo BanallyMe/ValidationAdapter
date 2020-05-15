01de204a79837c34fbe8b664da246fc97fb58ce4
BanallyMe <admin@eggrade.de>
2020-05-15 13:08:44 +0200

    Implemented swashbuckle operation filter for adding 422 Response

---
+++ ValidationAdapter/ValidationAdapter.AspNetCore.Swashbuckle.UnitTests/OperationFilters/AutoValidationOperationFilterTests.cs
@@ -0,0 +1,117 @@
+﻿using BanallyMe.ValidationAdapter.AspNetCore.ActionFilters.AutomaticValidation;
+using BanallyMe.ValidationAdapter.AspNetCore.ControllerOutput;
+using BanallyMe.ValidationAdapter.AspNetCore.Swashbuckle.OperationFilters;
+using FluentAssertions;
+using Microsoft.OpenApi.Models;
+using Moq;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace BanallyMe.ValidationAdapter.AspNetCore.Swashbuckle.UnitTests.OperationFilters
+{
+    public class AutoValidationOperationFilterTests
+    {
+        private readonly IOperationFilter testedFilter;
+        private readonly Mock<ISchemaGenerator> schemaGenerator;
+
+        public AutoValidationOperationFilterTests()
+        {
+            testedFilter = new AutoValidationOperationFilter();
+            schemaGenerator = new Mock<ISchemaGenerator>();
+        }
+
+        [Fact]
+        public void Apply_Adds422ResponseIfActionDecoratedWithAutoValidateAttribute()
+        {
+            Assert422ResponseIsAddedForMethod(GetAutoValidateDecoratedMethod());
+        }
+
+        [Fact]
+        public void Apply_Adds422ResponseIfControllerDecoratedWithAutoValidateAttribute()
+        {
+            Assert422ResponseIsAddedForMethod(GetMethodFromAutoValidateDecoratedClass());
+        }
+
+        private void Assert422ResponseIsAddedForMethod(MethodInfo method)
+        {
+            var fakeContext = new OperationFilterContext(null, schemaGenerator.Object, null, method);
+            var fakeSchema = new OpenApiSchema();
+            schemaGenerator.Setup(gen => gen.GenerateSchema(typeof(SerializableValidationResult), fakeContext.SchemaRepository, null, null)).Returns(fakeSchema);
+            var fakeOperation = new OpenApiOperation { Responses = new OpenApiResponses() };
+            var expectedAppendedSchema = new OpenApiSchema { Type = "array", Items = fakeSchema };
+
+            testedFilter.Apply(fakeOperation, fakeContext);
+
+            fakeOperation.Responses.Should().ContainKey("422");
+            fakeOperation.Responses["422"].Content.Should().ContainKey("application/json");
+            fakeOperation.Responses["422"].Description.Should().Be("Validation of the model failed.");
+            fakeOperation.Responses["422"].Content["application/json"].Schema.Should().BeEquivalentTo(expectedAppendedSchema);
+        }
+
+        [Fact]
+        public void Apply_DoesntAdd422ResponseIfActionAlreadyHas422Response()
+        {
+
+            var fakeApiResponses = new OpenApiResponses { ["422"] = new OpenApiResponse() };
+            var fakeOperation = new OpenApiOperation { Responses = fakeApiResponses };
+            var fakeContext = new OperationFilterContext(null, schemaGenerator.Object, null, GetAutoValidateDecoratedMethod());
+
+            var fakeSchema = new OpenApiSchema();
+            schemaGenerator.Setup(gen => gen.GenerateSchema(typeof(SerializableValidationResult), fakeContext.SchemaRepository, null, null)).Returns(fakeSchema);
+
+            var expectedApiResponses = fakeApiResponses.ToArray();
+
+            testedFilter.Apply(fakeOperation, fakeContext);
+
+            fakeOperation.Responses.Should().BeEquivalentTo(expectedApiResponses);
+        }
+
+        [Fact]
+        public void Apply_DoesntAdd422ResponseIfActionAndControllerNotDecoratedWithAutoValidateAttribute()
+        {
+            var fakeOperation = new OpenApiOperation();
+            var fakeContext = new OperationFilterContext(null, null, null, typeof(FakeControllerWithoutAutoValidate).GetMethod(nameof(FakeControllerWithoutAutoValidate.FakeActionWithoutAutoValidate)));
+
+            testedFilter.Apply(fakeOperation, fakeContext);
+
+            fakeOperation.Responses.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Apply_DoesntThrowExceptionIfOperationIsNull()
+        {
+            Action applying = () => testedFilter.Apply(null, new OperationFilterContext(null, null, null, null));
+
+            applying.Should().NotThrow();
+        }
+
+        [Fact]
+        public void Apply_DoesntThrowExceptionIfContextIsNull()
+        {
+            Action applying = () => testedFilter.Apply(Mock.Of<OpenApiOperation>(), null);
+
+            applying.Should().NotThrow();
+        }
+
+        private MethodInfo GetAutoValidateDecoratedMethod()
+            => typeof(FakeControllerWithoutAutoValidate).GetMethod(nameof(FakeControllerWithoutAutoValidate.FakeActionWithAutoValidate));
+
+        private MethodInfo GetMethodFromAutoValidateDecoratedClass()
+            => typeof(FakeControllerWithAutoValidate).GetMethod(nameof(FakeControllerWithAutoValidate.FakeActionWithoutAutoValidate));
+
+
+        private class FakeControllerWithoutAutoValidate
+        {
+            [AutoValidate]
+            public void FakeActionWithAutoValidate() { }
+
+            public void FakeActionWithoutAutoValidate() { }
+        }
+
+        [AutoValidate]
+        private class FakeControllerWithAutoValidate : FakeControllerWithoutAutoValidate { }
+    }
+}
---
+++ ValidationAdapter/ValidationAdapter.AspNetCore.Swashbuckle/OperationFilters/AutoValidationOperationFilter.cs
@@ -0,0 +1,55 @@
+﻿using BanallyMe.ValidationAdapter.AspNetCore.ActionFilters.AutomaticValidation;
+using BanallyMe.ValidationAdapter.AspNetCore.ControllerOutput;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BanallyMe.ValidationAdapter.AspNetCore.Swashbuckle.OperationFilters
+{
+    /// <summary>
+    /// Swashbuckle operation filter for adding validation response to all actions which are decorated
+    /// with the <see cref="AutoValidateAttribute"/>
+    /// </summary>
+    public class AutoValidationOperationFilter : IOperationFilter
+    {
+        /// <summary>
+        /// Adds possibility of 422 response to all actions decorated with <see cref="AutoValidateAttribute"/>
+        /// </summary>
+        /// <param name="operation">The swashbuckle operation that is being documented.</param>
+        /// <param name="context">Context in which this filter has been called.</param>
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation is null || context is null) return;
+
+            if (ContextHasAutoValidateAttributes(context))
+            {
+                var validationErrorSchema = GetSchemaForValidationResultArrayFromContext(context);
+                AddValidationResponseToOperationWithSchema(operation, validationErrorSchema);
+            }
+        }
+
+        private void AddValidationResponseToOperationWithSchema(OpenApiOperation operation, OpenApiSchema schema)
+        {
+            if (operation is null || operation.Responses.ContainsKey("422")) return;
+
+            operation.Responses.Add("422", new OpenApiResponse { Description = "Validation of the model failed.", Content = new Dictionary<string, OpenApiMediaType> { ["application/json"] = new OpenApiMediaType { Schema = schema } } });
+        }
+
+        private bool ContextHasAutoValidateAttributes(OperationFilterContext context)
+            => GetAutoValidateAttributesFromContext(context).Any();
+
+        private IEnumerable<AutoValidateAttribute> GetAutoValidateAttributesFromContext(OperationFilterContext context)
+            => context.MethodInfo.ReflectedType.GetCustomAttributes(true)
+                .Union(context.MethodInfo.GetCustomAttributes(true))
+                .OfType<AutoValidateAttribute>();
+
+        private OpenApiSchema GetSchemaForValidationResultArrayFromContext(OperationFilterContext context)
+        {
+            var validationResultSchema = context.SchemaGenerator.GenerateSchema(typeof(SerializableValidationResult), context.SchemaRepository);
+            var arraySchema = new OpenApiSchema { Type = "array", Items = validationResultSchema };
+
+            return arraySchema;
+        }
+    }
+}
